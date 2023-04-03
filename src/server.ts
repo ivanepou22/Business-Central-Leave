c1ba4cf198e293d647be752846688299076f012f
Ivan Epou <ivanepou92@gmail.com>
2023-04-03 14:16:08 +0300

    Add project structure and dependencies

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app: express.Application = express();
+const PORT = process.env.PORT;
+const address: string = `127.0.0.1:${PORT}`;
+
+app.use(bodyParser.json());
+
+app.get('/', function (_req: Request, res: Response) {
+  res.send('Hello Business Central');
+});
+
+//app.use('/api/v1', routes);
+
+app.listen(PORT, function () {
+  console.log(`starting app on: ${address}`);
+});
+
+export default app;
