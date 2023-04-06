d41084f9dca9fd38998ff0afc7abbcf474eb5801
Ivan Epou <ivanepou92@gmail.com>
2023-04-06 14:46:01 +0300

    Add database migrations

---
+++ migrations/sqls/20230406112635-users-table-up.sql
@@ -0,0 +1,14 @@
+/* Replace with your SQL commands */
+CREATE TYPE roles AS ENUM ('user', 'admin');
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(50),
+    role roles,
+    employeeNo VARCHAR(50),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
