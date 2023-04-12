16745ee6cf70f7e156d1e364d69b0b7ebdf37c63
Ivan Epou <ivanepou92@gmail.com>
2023-04-12 15:35:58 +0300

    Delete server directory

--- server/src/routes/employee.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getEmployees,getEmployeeNo } from '../handlers/employee.controller';
-
-const employees = express.Router();
-
-employees.get('/employees', getEmployees);
-employees.get('/employees/:id', getEmployeeNo);
-
-export default employees;
