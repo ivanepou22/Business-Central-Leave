21f0fecc2a8b964cd3915cf48ff7cfcc2049f3aa
Ivan Epou <ivanepou92@gmail.com>
2023-04-15 13:49:09 +0300

    Add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Login from './pages/Login'
 
 function App() {
   return (
     <div className="App">
-      <h1>MD365 Business Central</h1>
+      <Login/>
     </div>
   )
 }
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import portLogo from '../assets/images/logo-bg.png';
+import '../assets/login.css';
+
+function Login() {
+    return (
+        <div>
+            <div className="row g-0 app-auth-wrapper">
+                <div className="col-12 col-md-7 col-lg-6 auth-main-col text-center p-5">
+                    <div className="d-flex flex-column align-content-end">
+                        <div className="app-auth-body mx-auto">
+                            <div className="app-auth-branding mb-4" ><Link className="app-logo" to="#">
+                                <img className="logo-icon me-2" src={portLogo} alt="logo" /></Link></div>
+                            <h2 className="auth-heading text-center mb-5 login-title">Log in to Portal</h2>
+                            <div className="auth-form-container text-start">
+                                <form className="auth-form login-form">
+                                    <div className="email mb-3">
+                                        <label className="sr-only" htmlFor="signin-email">Email</label>
+                                        <input id="signin-email" name="signin-email" type="email" className="form-control signin-email" placeholder="Email address" required="required" />
+                                    </div>
+                                    <div className="password mb-3">
+                                        <label className="sr-only" htmlFor="signin-password">Password</label>
+                                        <input id="signin-password" name="signin-password" type="password" className="form-control signin-password" placeholder="Password" required="required" />
+                                        <div className="extra mt-3 row justify-content-between">
+                                            <div className="col-6">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="checkbox" value="" id="RememberPassword" />
+                                                    <label className="form-check-label" htmlFor="RememberPassword">
+                                                        Remember me
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="forgot-password text-end">
+                                                    <Link to="#">Forgot password?</Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="text-center">
+                                        <button type="submit" className="btn app-btn-primary w-100 theme-btn mx-auto">Log In</button>
+                                    </div>
+                                </form>
+
+                                <div className="auth-option text-center pt-5">No Account? Sign up <Link className="text-link" to="#" >here</Link>.</div>
+                            </div>
+
+                        </div>
+
+                        <footer className="app-auth-footer">
+                            <div className="container text-center py-3">
+                                <small className="copyright">Designed <i className="fas fa-heart"></i> by <Link className="app-link" to="https://meevolsoft.com/" target="_blank">MeevolSoft Ltd</Link></small>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+                <div className="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
+                    <div className="auth-background-holder">
+                    </div>
+                    <div className="auth-background-mask"></div>
+                    <div className="auth-background-overlay p-3 p-lg-5">
+                        <div className="d-flex flex-column align-content-end h-100">
+                            <div className="h-100"></div>
+                            <div className="overlay-content p-3 p-lg-4 rounded">
+                                <h5 className="mb-3 overlay-title"></h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
