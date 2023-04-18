0ad67e784098af6426fe46bac203a79d4f39f627
Ivan Epou <ivanepou92@gmail.com>
2023-04-18 23:24:00 +0300

    Add access

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import routes from './routes';
@@ -9,6 +9,13 @@ const PORT = process.env.PORT;
 const address: string = `127.0.0.1:${PORT}`;
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/', function (_req: Request, res: Response) {
   res.send('Hello Business Central');
