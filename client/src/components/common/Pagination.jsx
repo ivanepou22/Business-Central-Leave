db1cbfbfe093a65d5497054299ddaae06520b6f2
Ivan Epou <ivanepou92@gmail.com>
2023-04-25 16:48:06 +0300

    Optimize pagination

--- client/src/components/common/Pagination.jsx
+++ client/src/components/common/Pagination.jsx
@@ -23,21 +23,32 @@ function Pagination(props) {
     }
 
     return (
-        <nav aria-label="Page navigation example">
-            <ul className="pagination">
-                <li className={'page-item'}>
-                    <a className="page-link" onClick={handlePrevious}>{'<<'}</a>
-                </li>
-                {pages.map(page => (
-                    <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
-                        <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
+        <>
+            <div className="card-footer d-flex align-items-center">
+                <p className="m-0 text-muted">Showing <span>1</span> to <span>8</span> of <span>16</span> entries</p>
+                <ul className="pagination m-0 ms-auto">
+                    <li className="page-item">
+                        <a className="page-link" onClick={handlePrevious}>
+                            {/* {/* Download SVG icon from http://tabler-icons.io/i/chevron-left */}
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><polyline points="15 6 9 12 15 18" /></svg>
+                        </a>
                     </li>
-                ))}
-                <li className={'page-item'}>
-                    <a className="page-link" onClick={handleNext}>{'>>'}</a>
-                </li>
-            </ul>
-        </nav>
+
+                    {pages.map(page => (
+                        <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
+                            <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
+                        </li>
+                    ))}
+
+                    <li className="page-item">
+                        <a className="page-link" onClick={handleNext}>
+                            {/* next {/* Download SVG icon from http://tabler-icons.io/i/chevron-right */}
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><polyline points="9 6 15 12 9 18" /></svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 }
 
