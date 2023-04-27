c4ee3aeb6e1f01b6cc000038eba0222a4df156eb
Ivan Epou <ivanepou92@gmail.com>
2023-04-27 18:24:01 +0300

    Update the server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ const address: string = `127.0.0.1:${PORT}`;
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
